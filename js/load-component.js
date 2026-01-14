7b374659793a9db10a71a1d8f66c725d2908a0df
binoart <commissionilailatronci@outlook.com>
2026-01-14 18:25:02 +0100

    Update load-component.js

--- js/load-component.js
+++ js/load-component.js
@@ -15,11 +15,12 @@ async function loadComponent(elementId, filePath) {
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await loadComponent('header-placeholder', '/components/header.html');
-    await loadComponent('footer-placeholder', '/components/footer.html');
+    await loadComponent('header-placeholder', './components/header.html');
+    await loadComponent('footer-placeholder', './components/footer.html');
     const burgerMenuScript = document.createElement('script');
     burgerMenuScript.src = '/js/burger-menu.js';
     burgerMenuScript.type = 'module';
     document.body.appendChild(burgerMenuScript);
+
 
 });
